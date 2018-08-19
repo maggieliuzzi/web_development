2674fcdb0692805e5116bc20186cc6fec99e6f3b
MJClarke93 <Mitchell.J.Clarke@student.uts.edu.au>
2018-08-20 09:36:09 +1000

    Added temporary NewsPost component

--- src/Main.js
+++ src/Main.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
 
+class NewsPost extends Component {
+	render() {
+		return (
+			<div class='main-post'>
+				<p><b>Post Heading</b></p>
+				<p>Post description here...</p>
+			</div>
+		);
+	}
+}
+
 export default class Main extends Component {
   render() {
     return (
 		<div class='page-main'>
 		  <p>This is the main page!</p>
+		  <NewsPost />
+		  <NewsPost />
+		  <NewsPost />
+		  <NewsPost />
+		  <NewsPost />
 		</div>
     );
   }
