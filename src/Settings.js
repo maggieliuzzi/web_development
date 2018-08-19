0dc249caeaee133772372b03a1ab36fdfa9c2400
MJClarke93 <Mitchell.J.Clarke@student.uts.edu.au>
2018-08-20 09:52:40 +1000

    Added temporary settings form

--- src/Settings.js
+++ src/Settings.js
@@ -5,6 +5,13 @@ export default class Settings extends Component {
     return (
 		<div class='page-settings'>
 		  <p>This is the settings page!</p>
+		  <form>
+			<b>Profile Settings</b>
+			<br/><br/>Setting 1: <input type="text"/>
+			<br/>Setting 2: <input type="checkbox"/>
+			<br/>Setting 3: <input type="checkbox"/>
+			<br/><br/><input type="submit" value="Submit"/>
+		  </form>
 		</div>
     );
   }
