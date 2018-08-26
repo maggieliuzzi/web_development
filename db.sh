4551375bd333fab85c135e03742d0b565dd64386
MJClarke93 <Mitchell.J.Clarke@student.uts.edu.au>
2018-08-27 08:42:41 +1000

    Consolidated db.js and main.js

---
+++ db.sh
@@ -0,0 +1,2 @@
+mkdir ./db/data
+mongod --dbpath ./db/data
--- server/main.js
+++ server/main.js
@@ -1,29 +1,94 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
 
+// Connects Mongoose to the database "Up2Date" in MongoDB via localhost:27017
+mongoose.connect('mongodb://localhost:27017/Up2Date', { useNewUrlParser: true });
+
+// Moongoose defines the schema for our database
+const User = mongoose.model('User', {
+    username: {type: String, index: true},
+    password: String,
+    email: String,
+});
+const NewsPost = mongoose.model('NewsPost', {
+	source: String,
+	author: String,
+	body: String,
+	date: String
+});
+
+// Populate the database with dummy data
+User
+    .remove({})
+    .then(() => {
+        new User({username: 'marysmith', password: 'aip2021', email: 'mary.smith@gmail.com'}).save();
+		new User({username: 'mitchclarke', password: 'swordfish', email: 'mitch.clarke@gmail.com'}).save();
+    });
+NewsPost
+    .remove({})
+	.then(() => {
+		new NewsPost({
+			source: 'NewsSite.com',
+			author: 'Harry Bernstein',
+			body: "An important advancement in science has occured today, result in...",
+			date: "2018-7-20"}).save();
+		new NewsPost({
+			source: 'Twitter',
+			author: '@Banana',
+			body: "This is a very important Twitter post. Please pay attention.",
+			date: "2018-7-19"}).save();
+		new NewsPost({
+			source: 'NewsSite.com',
+			author: 'Greg Fields',
+			body: "A new species of beetle was discovered today in Australian rainforests...",
+			date: "2018-7-17"}).save();
+		new NewsPost({
+			source: 'Facebook',
+			author: 'Mitchell Clarke',
+			body: "Advanced Internet Programming is the best class ever! I've learnt so much!",
+			date: "2018-7-17"}).save();
+		new NewsPost({
+			source: 'NewsSite.com',
+			author: 'John Smith',
+			body: "10 ways in which robots are dooming humanity! Number 9 will absolutely shock you!",
+			date: "2018-7-16"}).save();
+	});
+
+// Prepares the Express app
 const app = express();
 
+// Enables body-parser to parse JSON and URLEncoded streams
 app.use(express.json());
 app.use(express.urlencoded());
 
-app.get("/api", (req, res) => {
-  res.json({
-    id: 1,
-    username: "test",
-    password: "blah"
-  });
-});
-
+// Dummy React->Express API handler; meant to accept data from the Login page's form
 app.post("/api", (req, res) => {
   var username = req.body.username;
   var password = req.body.password;
   console.log("Username: " + username + ", Password: " + password);
 });
 
+// Returns all of the users in the database
+app.get('/users', (req, res) => {
+    User
+        .find()
+        .then(results => res.json(results));
+});
+
+// Returns all of the news posts in the database
+app.get('/newsposts', (req, res) => {
+    NewsPost
+        .find()
+        .then(results => res.json(results));
+});
+
+// Connections to unknown locations return a 404
 app.all("*", (req, res) => {
   res.sendStatus(404);
 });
 
+// Makes the app listen to localhost:3001
 app.listen(3001, () => {
   console.log("Express server established and listening to http://localhost:3001");
 });
