cd2b71d75a54d46a86a99e2430f0cc709cfa2421
MJClarke93 <Mitchell.J.Clarke@student.uts.edu.au>
2018-10-02 15:01:48 +1000

    Added delete and update functionality; basic REST API done

--- server/main.js
+++ server/main.js
@@ -5,83 +5,29 @@ var io = require("socket.io")(http);
 var bodyParser = require("body-parser");
 
 
-// -----------------------------------
+// --------------------------------------------------------
 // SOCKET.IO
-// -----------------------------------
+// --------------------------------------------------------
 
 io.on("connection", function(socket) {
   console.log("Connection detected!");
 });
 
 function genSamplePost() {
-  titles = [
-    "My Post",
-    "Test Post",
-    "Greeting",
-    "New Status",
-    "My Day",
-    "New Entry",
-    "Blog Post",
-    "Reporting In"
-  ];
-  names = [
-    "Mitchell",
-    "Maggie",
-    "Abhusha",
-    "John",
-    "Carol",
-    "Mark",
-    "Harry",
-    "Jessie",
-    "Mark",
-    "Adrian",
-    "Bob",
-    "Tony",
-    "Sarah"
-  ];
-  bodies = [
-    "Nice day today.",
-    "I love AIP!",
-    "Anyone out there?",
-    "Work is hard...",
-    "Just testing things out.",
-    "Greetings everyone!",
-    "Check this out...",
-    "I'm the best :)",
-    "Oops..."
-  ];
-  sources = [
-    "Facebook",
-    "Twitter",
-    "News.com",
-    "Mag.com",
-    "Up2Date",
-    "Instagram",
-    "Chatter.com",
-    "Tumblr"
-  ];
-  dates = [
-    "21/3/18",
-    "15/4/18",
-    "7/5/18",
-    "27/7/18",
-    "1/8/18",
-    "19/9/18",
-    "14/3/18",
-    "17/4/18",
-    "29/5/18",
-    "5/7/18",
-    "6/8/18",
-    "8/9/18"
-  ];
-  i = Math.random()
-    .toString(36)
-    .slice(2);
+  titles = ["My Post","Test Post","Greeting","New Status","My Day","New Entry","Blog Post","Reporting In"];
+  names = ["Mitchell","Maggie","Abhusha","John","Carol","Mark","Harry","Jessie","Mark","Adrian","Bob","Tony","Sarah"];
+  bodies = ["Nice day today.","I love AIP!","Anyone out there?","Work is hard...","Just testing things out.",
+  "Greetings everyone!","Check this out...","I'm the best :)","Oops..."];
+  sources = ["Facebook","Twitter","News.com","Mag.com","Up2Date","Instagram","Chatter.com","Tumblr"];
+  dates = ["21/3/18","15/4/18","7/5/18","27/7/18","1/8/18","19/9/18","14/3/18","17/4/18","29/5/18","5/7/18","6/8/18","8/9/18"];
+
+  i = Math.random().toString(36).slice(2);
   t = titles[Math.floor(Math.random() * titles.length)];
   n = names[Math.floor(Math.random() * names.length)];
   b = bodies[Math.floor(Math.random() * bodies.length)];
   s = sources[Math.floor(Math.random() * sources.length)];
   d = dates[Math.floor(Math.random() * dates.length)];
+
   io.emit("samplePost", {
     id: i,
     title: t,
@@ -94,9 +40,9 @@ function genSamplePost() {
 setInterval(genSamplePost, 3000);
 
 
-// -----------------------------------
+// --------------------------------------------------------
 // EXPRESS
-// -----------------------------------
+// --------------------------------------------------------
 
 // Enables body-parser to parse JSON and URLEncoded streams
 app.use(express.json());
@@ -104,39 +50,176 @@ app.use(express.urlencoded({ extended: true }));
 
 // Connects to the MongoDB database
 var db = require("./db");
-var m = db.user_db(true);
+var m = db.user_db(true, true);
 
-// Responds to POST at "/api/new" by adding a user to the database
-app.post("/api/new", (req, res) => {
+
+// -----------------
+// GET-ing resources
+// -----------------
+
+// Reponds to GET at "/api" by returning a user's information
+app.get("/api", (req, res) => {
+  // Not implemented, as it isn't necessary (and would be a security risk)
+  res.sendStatus(501);
+});
+
+// Reponds to GET at "/api/creds" by returning a user's credentials
+app.get("/api/creds", (req, res) => {
+  // Not implemented, as it isn't necessary (and would be a security risk)
+  res.sendStatus(501);
+});
+
+// Responds to GET at "/api/prefs" by returning a user's preferences
+app.get("/api/prefs/:username", (req, res) => {
+  var username = req.params.username;
+  if (username) {
+    db.user_get_prefs(m, username, (result, success, errmsg) => {
+      if (success) {
+        res.json({success: true, error: null, result: result});
+      } else {
+        res.json({success: false, error: errmsg, result: null});
+      }
+    });
+  } else {
+    res.sendStatus(400);
+  }
+});
+
+
+// ------------------
+// POST-ing resources
+// ------------------
+
+// Responds to POST at "/api" by adding a user to the database
+app.post("/api", (req, res) => {
   var username = req.body.username;
   var password = req.body.password;
-  var email = "placeholder@mail.com";
-  db.user_add(m, username, email, password, (success, errmsg)=>{
-    if (success) {
-      res.json({success: true, error: null});
-    } else {
-      res.json({success: false, error: errmsg});
-    }
-  });
+  if (username && password) {
+    db.user_add(m, username, password, (success, errmsg)=>{
+      if (success) {
+        res.json({success: true, error: null});
+      } else {
+        res.json({success: false, error: errmsg});
+      }
+    });
+  } else {
+    res.sendStatus(400);
+  }
 });
 
-// Responds to POST at "/api/check" by checking if a user's credentials are correct
-app.post("/api/check", (req, res) => {
+// Responds to POST at "/api/creds" by checking if a user's credentials are correct
+app.post("/api/creds", (req, res) => {
   var username = req.body.username;
   var password = req.body.password;
-  db.user_check(m, username, password, (result, success, errmsg)=>{
-    if (success) {
-      res.json({success: true, error: null, result: result});
-    } else {
-      res.json({success: false, error: errmsg, result: null});
-    }
-  });
+  if (username && password) {
+    db.user_check(m, username, password, (result, success, errmsg)=>{
+      if (success) {
+        res.json({success: true, error: null, result: result});
+      } else {
+        res.json({success: false, error: errmsg, result: null});
+      }
+    });
+  } else {
+    res.sendStatus(400);
+  }
 });
 
-// Responds to requests at "/api", confirming it's the API root
-app.all("/api", (req, res) => {
-  res.send("This is the api root.");
+// Reponds to POST at "/api/prefs" by checking if a user's preferences match those in the request
+app.get("/api/prefs", (req, res) => {
+  // Not implemented, as it isn't necessary
+  res.sendStatus(501);
 });
+
+
+// --------------------
+// DELETE-ing resources
+// --------------------
+
+// Responds to DELETE at "/api" and similar by deleting the user from the database
+app.delete(["/api", "/api/creds", "/api/prefs"], (req, res) => {
+  var username = req.body.username;
+  var password = req.body.password;
+  if (username && password) {
+    db.user_check(m, username, password, (result, success, errmsg) => {
+      if (success) {
+        if (result) {
+          db.user_delete(m, username, (del_success, del_errmsg)=>{
+            if (del_success) {
+              res.json({success: true, error: null});
+            } else {
+              res.json({success: false, error: del_errmsg});
+            }
+          });
+        } else {
+          res.json({success: false, error: "Provided username and password did not match"});
+        }
+      } else {
+        res.json({success: false, error: errmsg});
+      }
+    });
+  } else {
+    res.sendStatus(400);
+  }
+});
+
+
+// --------------------------
+// PUT-ing (update) resources
+// --------------------------
+
+// Reponds to PUT at "/api"
+app.put("/api", (req, res) => {
+  // Not implemented, as PUT requests should target creds or prefs in particular
+  res.sendStatus(501);
+});
+
+app.put("/api/creds", (req, res) => {
+  username = req.body.username;
+  old_password = req.body.password;
+  new_password = req.body.new_password;
+  if (username && old_password && new_password) {
+    db.user_check(m, username, old_password, (result, success, errmsg) => {
+      if (success) {
+        if (result) {
+          db.user_update_creds(m, username, new_password, (success_new, errmsg_new) => {
+            if (success_new) {
+              res.send({success: true, error: null});
+            } else {
+              res.send({success: false, error: errmsg_new});
+            }
+          });
+        } else {
+          res.send({success: false, error: "Provided username and password did not match"});
+        }
+      } else {
+        res.send({success: false, error: errmsg});
+      }
+    });
+  } else {
+    res.sendStatus(400);
+  }
+});
+
+app.put("/api/prefs", (req, res) => {
+  username = req.body.username;
+  new_tags = req.body.tags;
+  if (username && new_tags) {
+    db.user_update_prefs(m, username, new_tags, (success, errmsg) => {
+      if (success) {
+        res.send({success: true, error: null});
+      } else {
+        res.send({success: false, error: errmsg});
+      }
+    });
+  } else {
+    res.sendStatus(400);
+  }
+});
+
+
+// -----
+// OTHER
+// -----
 
 // Responds to requests at "/all", confirming it's the server root
 app.all("/", (req, res) => {
