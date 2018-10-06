4bb00cf609d88ecaed2011be748aa12c92895bc6
Maggie Liuzzi <maggieliuzzi@192-168-1-9.tpgi.com.au>
2018-10-06 14:02:31 +1000

    Optimising dotenv deployment

--- server/newsAPI_config.js
+++ server/newsAPI_config.js
@@ -1,4 +1,4 @@
 require('dotenv').config();
 module.exports = {
-  newsAPI_key:           process.env.NEWSAPI_KEY // "ea46ce3aefa943889116a07b0caa4ee9"
+  newsAPI_key:           process.env.NEWSAPI_KEY
 }
--- server/twitterAPI_config.js
+++ server/twitterAPI_config.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 module.exports = {
-  consumer_key:         process.env.CONSUMER_KEY, // instead of consumer_key: "jasaskdjakls"
+  consumer_key:         process.env.CONSUMER_KEY,
   consumer_secret:      process.env.CONSUMER_SECRET,
   access_token:         process.env.ACCESS_TOKEN,
   access_token_secret:  process.env.ACCESS_TOKEN_SECRET,
