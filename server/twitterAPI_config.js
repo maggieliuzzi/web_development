b8f8c435d2ab6ab3a3c65dce7b0d3e1b46f538a0
Maggie Liuzzi <maggieliuzzi@192-168-1-9.tpgi.com.au>
2018-10-06 14:01:26 +1000

    Optimising dotenv deployment

--- server/twitterAPI_config.js
+++ server/twitterAPI_config.js
@@ -1,9 +1,9 @@
 require('dotenv').config();
 module.exports = {
-  consumer_key:         process.env.consumer_key, // instead of consumer_key: "jasaskdjakls"
-  consumer_secret:      process.env.consumer_secret,
-  access_token:         process.env.access_token,
-  access_token_secret:  process.env.access_token_secret,
+  consumer_key:         process.env.CONSUMER_KEY, // instead of consumer_key: "jasaskdjakls"
+  consumer_secret:      process.env.CONSUMER_SECRET,
+  access_token:         process.env.ACCESS_TOKEN,
+  access_token_secret:  process.env.ACCESS_TOKEN_SECRET,
   // timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
   // strictSSL:            true     // optional - requires SSL certificates to be valid.
 }
